bc41ecf75547a0468e5ce3de09c5c14c3f0d5605
githubsonoda <y274jp@yahoo.co.jp>
2015-09-20 04:43:15 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,32 @@ class User < ActiveRecord::Base
   validates :area, length: { maximum: 50 } # サインアップ時は未登録の為、空を許容
   
   has_many :microposts
+  
+  # k【フォローしている人】の実装
+  has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
+  
+  # 【フォローされている人】の実装
+  has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :follower_users, through: :follower_relationships, source: :follower
+  
+  # メソッド
+  # 他のユーザーをフォローする
+  def follow(other_user)
+    following_relationships.create(followed_id: other_user.id)
+  end
+
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
 end
